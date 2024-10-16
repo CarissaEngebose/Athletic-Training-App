ccc21c4f1e3ade11dc7304dd2f05ce7289b35bd1
Hannah <hannahhotchkiss08@gmail.com>
2024-10-16 14:47:29 -0500

    Modified the home screen and organized the screen files into appropriate folders

--- RecoveryAT/RecoveryAT/obj/Debug/net8.0-android/RecoveryAT.AssemblyInfo.cs
+++ RecoveryAT/RecoveryAT/obj/Debug/net8.0-android/RecoveryAT.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecoveryAT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3001ea7adce58526a6230d8ce011451eeefb394f")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecoveryAT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecoveryAT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
