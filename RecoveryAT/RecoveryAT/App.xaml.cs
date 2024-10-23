ac5ba34ac1e1ae25f9549bb83bae27b87f985570
Carissa Engbose <engeboseca90@uwosh.edu>
2024-10-22 22:09:38 -0500

    Edited the App page for log in and log out functionality.

--- RecoveryAT/RecoveryAT/App.xaml.cs
+++ RecoveryAT/RecoveryAT/App.xaml.cs
@@ -2,11 +2,12 @@
 
 public partial class App : Application
 {
+	public AuthenticationService AuthService { get; private set; } // creates an authentication service
 	public App()
 	{
 		InitializeComponent();
-
-		MainPage = new InjuryStatistics();
+        AuthService = new AuthenticationService(); // creates an authentication service instance
+        MainPage = new NavigationPage(new WelcomeScreen());
 
 	}
 }
