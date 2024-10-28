7e8cc9757825589b19d3f363222686ae97248d58
Carissa Engbose <engeboseca90@uwosh.edu>
2024-10-27 22:17:28 -0500

    Created an AuthenticationService file to keep track of whether a user is logged in or out to show the main tabbed bar or not.

--- RecoveryAT/RecoveryAT/Models/AuthenticationService.cs
+++ RecoveryAT/RecoveryAT/Models/AuthenticationService.cs
@@ -1,3 +1,13 @@
+/**
+    Name: Carissa Engebose
+    Date: 10/24/24
+    Description: Implementation for log in and log out services for the user. Right now, it is only used to show
+    the main tabbed bar or not because we haven't implemented create account or log in.
+    Bugs: None that I know of.
+    Reflection: This class didn't take long at all and was only used to display the main tabbed bar if the log in
+    or create account buttons were clicked.
+**/
+
 public class AuthenticationService {
     public bool IsLoggedIn { get; private set; }
 
