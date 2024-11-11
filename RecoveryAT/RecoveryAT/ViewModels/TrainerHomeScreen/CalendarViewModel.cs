10412a495cefc0ade843802e038dfe45443e2186
DominickHagedorn <53031881+DominickHagedorn@users.noreply.github.com>
2024-11-11 12:29:34 -0600

    Broke the TrainerHome viewmodel up to make things more readable

---
+++ RecoveryAT/RecoveryAT/ViewModels/TrainerHomeScreen/CalendarViewModel.cs
@@ -0,0 +1,41 @@
+using System.ComponentModel;
+using CalendarManagment;
+
+namespace RecoveryAT{
+    
+    public class CalendarViewModel : INotifyPropertyChanged
+    {
+        public List<String> Months;
+        private Date _selectedDate;
+        public Date SelectedDate {get => _selectedDate; set{_selectedDate = value; OnPropertyChanged();}}
+        public Action<DateTime> OnDaySelected;
+        public CalendarViewModel()
+        {
+            SelectedDate = new Date(DateTime.Now, OnDaySelected);
+            Months = new List<String>(Date.MonthNames);
+        }
+
+        public void SetNextWeek()
+        {
+            SelectedDate = SelectedDate.AddDays(7); // go to next week
+        }
+
+        public void SetPreviousWeek()
+        {
+            SelectedDate = SelectedDate.AddDays(-7); // go to previous week
+        }
+
+        public void SetMonth(int Month){
+            SelectedDate = SelectedDate.AddMonths(SelectedDate.Month - Month); // calculate new month
+        }
+
+        public void SetYear(int Year){
+            SelectedDate = SelectedDate.AddYears(SelectedDate.Year - Year); // calculate new year
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ RecoveryAT/RecoveryAT/ViewModels/TrainerHomeScreen/Date.cs
@@ -0,0 +1,52 @@
+namespace CalendarManagment
+{
+    // wrapper class of DateTime to make working with dates easier
+    public class Date
+    {
+        public static string[] DaysOfWeek = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+        public static string[] MonthNames = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+        private DateTime _date;
+        public int Month => _date.Month;
+        public int Year => _date.Year;
+        public int Day => _date.Day;
+        public int DayOfTheWeek => (int)_date.DayOfWeek;
+        public Date AddDays(double NumDays) => new Date(_date.AddDays(NumDays), OnNewDaySelected);
+        public Date AddMonths(int months) => new Date(_date.AddMonths(months), OnNewDaySelected);
+        public Date AddYears(int years) => new Date(_date.AddYears(years), OnNewDaySelected);
+        public List<Day> Week;
+        public Action<DateTime> OnNewDaySelected;
+        public Date(int Month, int Year, int Day, Action<DateTime> OnNewDaySelected) : this(new DateTime(Month, Year, Day), OnNewDaySelected)
+        {
+        }
+
+        public Date(DateTime NewDate, Action<DateTime> OnNewDaySelected){
+            _date = NewDate;
+            this.OnNewDaySelected = OnNewDaySelected;
+            Week = CalculateWeek();
+        }
+
+        private List<Day> CalculateWeek()
+        {
+            DateTime sundayDate = _date.AddDays(-DayOfTheWeek); // gets closest previous sunday
+            List<Day> daysOfTheWeek = new List<Day>(); // temporary storage of new days
+            for (int CurrDay = 0; CurrDay < 7; CurrDay++) // for each day in the week
+            {
+                Day NewDay = new Day(DaysOfWeek[CurrDay], sundayDate.AddDays(CurrDay).Day); // create a new day
+                daysOfTheWeek.Add(NewDay); // add new day to week
+                // set what happens when a new day is selected
+                NewDay.IsSelectedEvent += (NewSelectedDay) =>
+                {
+                    foreach (Day day in Week) // for each day in the week
+                    {
+                        if (!day.Equals(NewSelectedDay) && day.IsSelected) // if day isnt newly selected day
+                        {
+                            day.IsSelected = false; // deselect
+                        }
+                    }
+                    OnNewDaySelected?.Invoke(this._date);
+                };
+            }
+            return daysOfTheWeek;
+        }
+    }
+}
