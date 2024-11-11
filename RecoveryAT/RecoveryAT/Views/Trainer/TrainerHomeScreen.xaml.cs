e4350dd6a30b62f25d23cd702c513d1d07f434e5
DominickHagedorn <53031881+DominickHagedorn@users.noreply.github.com>
2024-11-11 17:13:36 -0600

    Made week get updated when year or month change (Finally)

--- RecoveryAT/RecoveryAT/Views/Trainer/TrainerHomeScreen.xaml.cs
+++ RecoveryAT/RecoveryAT/Views/Trainer/TrainerHomeScreen.xaml.cs
@@ -43,15 +43,11 @@ namespace RecoveryAT
         }
 
         public void MonthChanged(object sender, EventArgs e){
-            //Frame MonthFrame = (Frame)sender;
-            //String Month = (String)MonthFrame.BindingContext;
-            //ViewModel.Calendar.SetMonth(Month);
+            ViewModel.Calendar.SetMonth(MonthCarousel.Position);
         }
 
         public void YearChanged(object sender, EventArgs e){
-            //Frame YearFrame = (Frame)sender;
-            //String Year = (String)YearFrame.BindingContext;
-            //ViewModel.Calendar.SetYear(Int32.Parse(Year));
+            ViewModel.Calendar.SetYear(YearCarousel.Position+1950);
         }
     }
 }
